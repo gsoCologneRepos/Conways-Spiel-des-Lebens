2e434dbb119916d8fe01d486dd31522698282688
Rosario <rosario.polito@gmx.de>
2019-05-19 14:44:47 +0200

    BugFix & Formatierung

--- GoL/Starter.cs
+++ GoL/Starter.cs
@@ -7,31 +7,32 @@ namespace GoL
         //Diese Klasse startet das Spiel, generiert das Array und befüllt die Zellen
         public Cell[,] start(int groesse)
         {
-            Cell[,] spielfeld = new Cell[groesse,groesse];
-            return spielfeld;
-        }
-        public Cell[,] fill(Cell[,] spielfeld)
-        {
-            Random rnd = new Random();
-            for(int i=0;i<spielfeld.GetLength(0);i++)
-            {
-                for(int j=0;j<spielfeld.GetLength(0);j++)
-                {
-                    if (rnd.Next(1, 101) > 90)
-                    {
-                        spielfeld[i, j] = new Cell(true);   
-                    }
-                    else
-                    {
-                        spielfeld[i, j] = new Cell(false);    
-                    }
-                }
-                
-            }
+            Cell[,] spielfeld = new Cell[groesse, groesse];
             return spielfeld;
         }
 
-        public static void spielzug(Cell[,] spielfeld,Cell[,] spielfeldNeu,int xMaxs)
+        public Cell[,] fill(Cell[,] spielfeld)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < spielfeld.GetLength(0); i++)
+            {
+                for (int j = 0; j < spielfeld.GetLength(0); j++)
+                {
+                    if (rnd.Next(1, 101) > 90)
+                    {
+                        spielfeld[i, j] = new Cell(true);
+                    }
+                    else
+                    {
+                        spielfeld[i, j] = new Cell(false);
+                    }
+                }
+            }
+
+            return spielfeld;
+        }
+
+        public static void spielzug(Cell[,] spielfeld, Cell[,] spielfeldNeu, int xMax)
         {
             for (int x = 0; x < spielfeld.GetLength(0); x++)
             {
@@ -40,54 +41,55 @@ namespace GoL
                     //Oben Links
                     if (x == 0 && y == 0)
                     {
-                        Logik.topLeftCorner(spielfeld,spielfeldNeu);
+                        Logik.topLeftCorner(spielfeld, spielfeldNeu);
                     }
                     //Unten Rechts
                     else if (x == xMax && y == xMax)
                     {
-                        Logik.botRightCorner(spielfeld,spielfeldNeu,x);
+                        Logik.botRightCorner(spielfeld, spielfeldNeu, x);
                     }
                     //Unten Links
                     else if (x == xMax && y == 0)
                     {
-                        Logik.botLeftCorner(spielfeld,spielfeldNeu,x);
+                        Logik.botLeftCorner(spielfeld, spielfeldNeu, x);
                     }
-                    
+
                     // oben Rechts
-                    else if (x == 0 && y == xMax) 
+                    else if (x == 0 && y == xMax)
                     {
-                        Logik.topRightCorner(spielfeld,spielfeldNeu,x);
+                        Logik.topRightCorner(spielfeld, spielfeldNeu, x);
                     }
-                   
+
                     //linke Spalte
-                    else if(x!=0 && y== 0 && x!=xMax)
+                    else if (x != 0 && y == 0 && x != xMax)
                     {
-                        Logik.leftColumn(spielfeld,spielfeldNeu,x,y);
+                        Logik.leftColumn(spielfeld, spielfeldNeu, x, y);
                     }
-                    
+
                     //rechte Spalte
                     else if (x != 0 && y == xMax)
                     {
-                        Logik.rightColumn(spielfeld,spielfeldNeu,x,y);
+                        Logik.rightColumn(spielfeld, spielfeldNeu, x, y);
                     }
-                    
+
                     //oben
-                    else if(x==0 && y!= 0)
+                    else if (x == 0 && y != 0)
                     {
-                        Logik.topRow(spielfeld,spielfeldNeu,x,y);
+                        Logik.topRow(spielfeld, spielfeldNeu, x, y);
                     }
-                    
+
                     //unten
-                    else if(x==xMax && y!= 0)
+                    else if (x == xMax && y != 0)
                     {
-                        Logik.botRow(spielfeld,spielfeldNeu,x,y);
+                        Logik.botRow(spielfeld, spielfeldNeu, x, y);
                     }
 
                     else
                     {
-                        Logik.fullCircle(spielfeld,spielfeldNeu,x,y);
+                        Logik.fullCircle(spielfeld, spielfeldNeu, x, y);
                     }
                 }
+            }
         }
     }
 }
